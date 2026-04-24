b43b3419cae3dcad072b1b478c63a1975fd811fb
Junin338 <pedrojuniorvictor923@gmail.com>
2026-04-24 08:57:37 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("Botao").onclick = function() {
+    alert("conheça a espiração do mascote da seleção brasileira meu amiginho o clebinho o canarinho!");
+    window.open("https://bairrobook.blogspot.com/2014/10/canario-ou-canarinho-vive-no-bairro-da.html", "_blank");
+};
+document.getElementById("Botao2").onclick = function() {
+    alert("conheça o Neymar!");
+    window.open("https://www.cnnbrasil.com.br/esportes/futebol/selecao-brasileira/neymar-fala-sobre-retorno-a-selecao-e-copa-tenho-algo-a-conquistar/", "_blank");
+};
+document.getElementById("Botao3").onclick = function() {
+    alert("conheça a seleção atual");
+    window.open("https://www.cnnbrasil.com.br/esportes/futebol/selecao-brasileira/idolo-da-selecao-analisa-grupo-da-copa-e-preve-rival-para-possivel-final/", "_blank");
+};
